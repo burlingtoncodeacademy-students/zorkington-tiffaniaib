497dc36e208231914f41d95a0ea997d027ebc360
[Tiffania] <ielenatiffania@gmail.com>
2023-05-01 12:58:50 -0400

    the game starts without issues

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ function ask(questionText) {
 
 
   
+//================================================================        STATE MACHINE      ===================================================================================
+//We need a State Machine to represent the different rooms and how they are connected
+// the keys (left) are the different locations, the values (right) represent the possible transitions for each room
+const locationsYouCanGo = {
+  street : ['hallway'],
+  hallway: ['street', 'kitchen', 'bedroom'],
+  bedroom: ['hallway'],
+  kitchen: ['hallway'],
+ };
+ 
+ // *****************************************************
+let currentPlayerLocation = 'street';
+// *****************************************************
 
 // #CLASSES
 
@@ -33,22 +46,43 @@ class Item {
 }
 
 // ==================================================================        ITEMS          ======================================================================================
-
+let sign = new Item (
+  'sign',
+  `The sign is a message from your parents: 'You know dad and I like trivia games, right?\nOur passcode is the number of continents.`,
+  '',
+  '',
+  '',
+)
 
 
 
 // ==================================================================        ROOMS         ======================================================================================
+let street = new Room (
+  'The keys are under the doormat. Mom',
+  locationsYouCanGo.street,
+  ['sign'],
+  false
+);
 
+let hallway = new Room (
+  `The hallway is just like you remembered: with white tinted walls and family pictures everywhere.`,
+  locationsYouCanGo.hallway,
+  ['library card'],
+  true
+)
 
-//================================================================        STATE MACHINE      ===================================================================================
-//We need a State Machine to represent the different rooms and how they are connected
-// the keys (left) are the different locations, the values (right) represent the possible transitions for each room
-const locationsYouCanGo = {
- street : ['hallway'],
- hallway: ['street', 'kitchen', 'bedroom'],
- bedroom: ['hallway'],
- kitchen: ['hallway'],
-};
+let kitchen = new Room (
+  `You go straight to the fridge to grab a bite of whatever is inside.\nThen you head towards the cabinet where your parents store the wine and you see a bottle of your favorite wine.\n`,
+   locationsYouCanGo.kitchen,
+   ['bottle of wine'],
+   true
+  )
+
+let bedroom = new Room(
+  `Your parents bedroom hasn't change one bit since last time you were there. Your mom told you that your dad's birthday gift is locked in a casket in her closet.\n the key to open it is as well in the closet, hidden inside a pink sock.`,
+  ['closet', 'keyCasket', 'gift'],
+  true
+)
 
 // ==========================================================        LOOKUP TABLES      ==================================================================================================
 
@@ -81,11 +115,8 @@ function pickItem(Item) {
 }
 
 function dropItem(Item) {
-  
+ player.inventory.pop(); 
 }
-
-
-
 
 function transition(newRoom) {
   const validTransitions = locationsYouCanGo[currentPlayerLocation];
@@ -117,28 +148,19 @@ function openItem(item) {
       console.log(`Perfect! Now the door opens.`);
     }   
   }
-  
-
-
-
-
-
-
-
 
 // ----------------------------------------------------
 let player = {
-  inventory: [],
-  status: null
+  inventory: ['telephone'],
+  status: currentPlayerLocation
 };
-// *****************************************************
-let currentPlayerLocation = 'street';
-// *****************************************************
 
 start();
 async function start() {
 
-  console.log(`182 Main St.
+  console.log(`You are on a mission to get your dad's gift from your parents house to his birthday party.\nI know, very serious stuff eh? What can I say? Batman was busy.
+  
+INTRO: 182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign`);
