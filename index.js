c75883eeaed32806ef4b005743c3d21e646c389c
[Tiffania] <ielenatiffania@gmail.com>
2023-05-01 11:55:16 -0400

    started from 0

--- index.js
+++ index.js
@@ -32,99 +32,22 @@ class Item {
   }
 }
 
-class CanGetOpened {
-  constructor (name, isOpen) {
-    this.name = name
-    this.isOpen = isOpen
-  }
-}
 // ==================================================================        ITEMS          ======================================================================================
-let sign = new Item (
-  'sign',
-  'This door is closed,\nthe password to unlock it is the number of continents.',
-  '',
-  '',
-  ''
-)
 
-let doorPass = '7';
-
-let parchment = new Item (
-  'parchment', //name
-`The content is the following: 
-'You know where you're coming from, but not where you are going.
-Yours is a mysterious journey, but by keeping in mind
-the following tips you should make it out alive. Hehe I'm kidding.
-But seriously, remember this:
-
-1. Use only two words when writing your commands,
-we are very lazy and don't read more than that;
-
-2. if you want to know what's inside the inventory, 
-use 'i' or 'inventory' followed by the name of the item that you want to check.
-That's all. GOOD LUCK!`, //description
-'', //inventory is empty
-'',
-''
-);
-
-
-let backpack = new Item (
-  "backpack",
-  "",
-  ['wallet', 'bottle of water'],
-  '',
-  '',
-  '' 
-);
 
 
 
 // ==================================================================        ROOMS         ======================================================================================
-const house = new Room (
-  '',
-  'field',
-  [],
-  false
-)
 
-const tree = new Room(
-  '',
-  ['field', 'castle'],
-  [],
-  true
-);
-
-const field = new Room(
- `You have entered the foyer and the door shuts behind you;
-What you see looks unbeliavebly beautiful:
-it's a huge field with bright green grass, that almost seems like it has no end.
-Ahead of you (north) there is a long pathway that leads to a giagantic tree.
-On your right (east), 10 feet from where you are,
-you can see an old brick house.
-While you are busy trying to figure out which way to go, you notice something rolling at your feet.
-It's a parchment.`, //description
-  ['street', 'tree', 'brick house'], //connection
-  [parchment], //inventory
-  null //isOpen
-  );
-
-const street = new Room(
-  [],
-  ['field'],
-  [sign],
-  false
-);
 
 //================================================================        STATE MACHINE      ===================================================================================
 //We need a State Machine to represent the different rooms and how they are connected
 // the keys (left) are the different locations, the values (right) represent the possible transitions for each room
 const locationsYouCanGo = {
- street : ['field'],
- field: ['tree', 'house'],
- brick: ['field'],
- tree: ['castle'],
- castle: ['street']
+ street : ['hallway'],
+ hallway: ['street', 'kitchen', 'bedroom'],
+ bedroom: ['hallway'],
+ kitchen: ['hallway'],
 };
 
 // ==========================================================        LOOKUP TABLES      ==================================================================================================
@@ -142,10 +65,7 @@ const commandLookup = {
 
 const roomLookup = {
   street: 'street',
-  field: 'field',
-  house: 'house',
-  tree: 'tree',
-  castle: 'castle'
+  hallway: 'hallway'
 }
 
  // ==============================================================       FUNCTIONS    ==============================================================================================
@@ -231,42 +151,40 @@ while(true) {
 
   if(commandLookup.move.includes(command)) {
     transition(thing)
-  } else if (commandLookup.inventory.includes(command)) {
-    getInventory(thing)
-
-  } else if (commandLookup.use.includes(command)) {
-    useItem(thing);
-
-  } else if (commandLookup.pickup.includes(command)) {
-    pickItem(thing);
+  } 
   
-  } else if (commandLookup.drop.includes(command)) {
+  else if (commandLookup.inventory.includes(command)) {
+    getInventory(thing)
+  } 
+  
+  else if (commandLookup.use.includes(command)) {
+    useItem(thing);
+  } 
+  
+  else if (commandLookup.pickup.includes(command)) {
+    pickItem(thing);
+  } 
+  
+  else if (commandLookup.drop.includes(command)) {
     // ...
-
-  } else if(commandLookup.help.includes(command)) {
+  } 
+  
+  else if(commandLookup.help.includes(command)) {
     console.log('put function here for help inventory')
+  } 
 
-  } else if(commandLookup.drop.includes(command)) {
+  else if(commandLookup.drop.includes(command)) {
     console.log('put function here for drop inventory')
   }
+
   else if(commandLookup.read.includes(command)) {
     if (thing.includes('sign')) {
         console.log(sign.description);
-    } 
-    else if (thing.includes('parchment')) {
-      console.log(parchment.description);
-    }
-   
-  } else if(commandLookup.open.includes(command)) {
-    openItem(thing);
-  
-// TO OPEN DOOR AT THE BEGINNING
+    }  
   } 
-  else if(command.includes('7')) {//if the command is the password
-    openItem(street.doorIsOpen); // use the function that opens doors to open the one at the beginning
-    transition(field);
-    console.log(curr)
-    console.log(field.description);
+
+  else if(commandLookup.open.includes(command)) {
+    openItem(thing);
   } else {
     console.log(`Are you sure about your answer? ${command} doesn't seem right.
 Try again`)
